adee56ec40bd012b3c6759961fc8cff52056b1bf
ashernamanya <asher@pesamoni.com>
2018-09-26 12:15:49 +0000

    Changes made to gemfile and home page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsProjects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,6 +20,21 @@ module RailsProjects
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # in config/application.rb after Bundler.require(..)
+
+# catch the incorrect pg version check, replace it with one that will pass
+module Kernel
+  def gem_with_pg_fix(dep, *reqs)
+    if dep == "pg" && reqs == ["~> 0.15"]
+      reqs = ["~> 1.0"]
+    end
+    gem_without_pg_fix(dep, *reqs)
+  end
+
+  alias_method_chain :gem, :pg_fix
+end
+# pg 1.0 gem has removed these constants, but 4.2 ActiveRecord still expects them
+
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
--- config/production.rb
+++ config/production.rb
@@ -1,1 +1,3 @@
  config.assets.compile = true
+ 
+ 
